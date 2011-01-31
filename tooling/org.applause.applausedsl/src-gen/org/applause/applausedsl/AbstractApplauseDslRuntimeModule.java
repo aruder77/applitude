fc11933cc85af1cbcf07b69611afa645287a0425
Ralf Ebert <info@ralfebert.de>
2011-01-31 01:38:54 +0100

    changed file extension from .app to .applitude with version 0.2.1

--- tooling/org.applause.applausedsl/src-gen/org/applause/applausedsl/AbstractApplauseDslRuntimeModule.java
+++ tooling/org.applause.applausedsl/src-gen/org/applause/applausedsl/AbstractApplauseDslRuntimeModule.java
@@ -31,7 +31,7 @@ public abstract class AbstractApplauseDslRuntimeModule extends DefaultRuntimeMod
 	
 	public void configureFileExtensions(Binder binder) {
 		if (properties == null || properties.getProperty(Constants.FILE_EXTENSIONS) == null)
-			binder.bind(String.class).annotatedWith(Names.named(Constants.FILE_EXTENSIONS)).toInstance("app");
+			binder.bind(String.class).annotatedWith(Names.named(Constants.FILE_EXTENSIONS)).toInstance("applitude");
 	}
 	
 	// contributed by org.eclipse.xtext.generator.grammarAccess.GrammarAccessFragment
