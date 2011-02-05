ad0ae3b16e69b5b8c30e698eb5b131430a0112be
Ralf Ebert <info@ralfebert.de>
2011-02-05 19:35:17 +0100

    better expression example

--- examples/demo/Generated/ReferenceExpressionsViewController.m
+++ examples/demo/Generated/ReferenceExpressionsViewController.m
@@ -16,15 +16,16 @@
 	[fInventors request];
 	self.title = @"Expressions";
 
-	[self sectionWithTitle:[NSString stringWithFormat:@"%@", @"String concat"]];
+	[self sectionWithTitle:[NSString stringWithFormat:@"%@", @"String manipulation"]];
 	{
 		[self cells:@selector(inventorCell:) forContentProvider:fInventors];
 	}
 }
 
 - (UITableViewCell *) inventorCell:(id)inventor {
-	BoxCell *cell = [[[BoxCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil] autorelease];
-	cell.textLabel.text = [NSString stringWithFormat:@"%@%@%@%@", [inventor valueForKey:@"name"], @" (", [inventor valueForKey:@"name"], @")"];
+	BoxCell *cell = [[[BoxCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil] autorelease];
+	cell.textLabel.text = [inventor valueForKey:@"name"];
+	cell.detailTextLabel.text = [NSString stringWithFormat:@"%@%@%@", [[inventor valueForKey:@"name"] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding], @", ", [[inventor valueForKey:@"name"] stringByReplacingOccurrencesOfString:@" " withString:@"_"]];
 	cell.data = inventor;
 	return cell;
 }
