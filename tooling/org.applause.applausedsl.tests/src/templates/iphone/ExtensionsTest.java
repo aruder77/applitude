26b3441835f11230deb8e388ee4d2478e41a3d2c
Ralf Ebert <info@ralfebert.de>
2011-02-07 12:21:19 +0100

    resolveToValue for ScalarExpressions

--- tooling/org.applause.applausedsl.tests/src/templates/iphone/ExtensionsTest.java
+++ tooling/org.applause.applausedsl.tests/src/templates/iphone/ExtensionsTest.java
@@ -8,13 +8,17 @@ import org.applause.applausedsl.applauseDsl.ApplauseDslFactory;
 import org.applause.applausedsl.applauseDsl.Cell;
 import org.applause.applausedsl.applauseDsl.CollectionIterator;
 import org.applause.applausedsl.applauseDsl.ComplexProviderConstruction;
-import org.applause.applausedsl.applauseDsl.ContentProvider;
 import org.applause.applausedsl.applauseDsl.Model;
 import org.applause.applausedsl.applauseDsl.ObjectReference;
 import org.applause.applausedsl.applauseDsl.Parameter;
 import org.applause.applausedsl.applauseDsl.Property;
+import org.applause.applausedsl.applauseDsl.ScalarExpression;
 import org.applause.applausedsl.applauseDsl.ScopeName;
 import org.applause.applausedsl.applauseDsl.SimpleProviderConstruction;
+import org.applause.applausedsl.applauseDsl.StringConcat;
+import org.applause.applausedsl.applauseDsl.StringLiteral;
+import org.applause.applausedsl.applauseDsl.StringReplace;
+import org.applause.applausedsl.applauseDsl.StringUrlConform;
 import org.applause.applausedsl.applauseDsl.TableView;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtend.XtendFacade;
@@ -34,7 +38,6 @@ public class ExtensionsTest extends AbstractXtextTests {
 	private Parameter parameterFoo;
 	private Property prop1;
 	private Property prop2;
-	private ContentProvider contentProvider;
 	private Model model;
 	private TableView table;
 	private ComplexProviderConstruction complexProviderConstruction;
@@ -60,8 +63,6 @@ public class ExtensionsTest extends AbstractXtextTests {
 
 		prop2 = ApplauseDslFactory.eINSTANCE.createProperty();
 		prop2.setName("prop2");
-
-		contentProvider = ApplauseDslFactory.eINSTANCE.createContentProvider();
 
 		table = Iterables.filter(model.eContents(), TableView.class).iterator().next();
 		complexProviderConstruction = (ComplexProviderConstruction) table.getVariables().get(0).getValue();
@@ -118,6 +119,69 @@ public class ExtensionsTest extends AbstractXtextTests {
 	}
 
 	@Test
+	public void testStringLiteralResolveToValue() throws Exception {
+		assertResolveToValue("@\"foo\"", str("foo"));
+	}
+
+	private StringLiteral str(String value) {
+		StringLiteral literal = ApplauseDslFactory.eINSTANCE.createStringLiteral();
+		literal.setValue(value);
+		return literal;
+	}
+
+	@Test
+	public void testStringConcatResolveToValue() throws Exception {
+		assertResolveToValue("[NSString stringWithFormat:@\"%@%@\", @\"foo\", @\"bar\"]", concat(str("foo"), str("bar")));
+	}
+
+	@Test
+	public void testStringConcatResolveToValueNested() throws Exception {
+		StringConcat concat = concat(str("foo"), str("bar"));
+		assertResolveToValue("[NSString stringWithFormat:@\"%@%@\", [NSString stringWithFormat:@\"%@%@\", @\"foo\", @\"bar\"], @\"mo\"]", concat(concat, str("mo")));
+	}
+
+	@Test
+	public void testStringUrlConformResolveToValue() throws Exception {
+		StringUrlConform expr = ApplauseDslFactory.eINSTANCE.createStringUrlConform();
+		expr.setValue(str("foo"));
+		assertResolveToValue("[@\"foo\" stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]", expr);
+	}
+
+	@Test
+	public void testStringReplaceResolveToValue() throws Exception {
+		StringReplace expr = ApplauseDslFactory.eINSTANCE.createStringReplace();
+		expr.setValue(str("foo"));
+		expr.setMatch(str("x"));
+		expr.setReplacement(str("y"));
+		assertResolveToValue("[@\"foo\" stringByReplacingOccurrencesOfString:@\"x\" withString:@\"y\"]", expr);
+	}
+
+	@Test
+	public void testNullResolveToValue() throws Exception {
+		assertResolveToValue("nil", null);
+	}
+
+	@Test
+	public void testObjectReferenceResolveToValue() throws Exception {
+		assertResolveToValue("fFoo.content",
+				ref(parameterFoo));
+		assertResolveToValue("[fFoo.content valueForKey:@\"prop1\"]",
+				ref(parameterFoo, prop1));
+		assertResolveToValue("[fFoo.content valueForKeyPath:@\"prop1.prop2\"]",
+				ref(parameterFoo, prop1, prop2));
+
+	}
+
+	private StringConcat concat(ScalarExpression... values) {
+		StringConcat concat = ApplauseDslFactory.eINSTANCE.createStringConcat();
+		for (ScalarExpression value : values) {
+			concat.getValues().add(value);
+		}
+		return concat;
+	}
+
+	
+	@Test
 	public void testProviderExpressionStraight() throws Exception {
 		assertProviderExpression("fInventors", 0);
 	}
@@ -141,8 +205,12 @@ public class ExtensionsTest extends AbstractXtextTests {
 		assertEquals(expectedExpression, xtend.call("providerExpression", iterator.getCollection()));
 	}
 
-	private void assertResolveToProvider(String expectedExpression, EObject object) {
-		assertEquals(expectedExpression, xtend.call("resolveToProvider", object));
+	private void assertResolveToProvider(String expected, EObject object) {
+		assertEquals(expected, xtend.call("resolveToProvider", object));
+	}
+
+	private void assertResolveToValue(String expected, EObject object) {
+		assertEquals(expected, xtend.call("resolveToValue", object));
 	}
 
 	private ObjectReference ref(ScopeName... path) {
